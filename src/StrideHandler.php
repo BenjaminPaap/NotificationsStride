603f3da2a712ca98dd31df9715e9b9df9cfef492
Benjamin Paap <benjamin.paap@gmail.com>
2018-04-23 22:51:25 +0200

    Add: Initial commit

---
+++ src/StrideHandler.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Bpa\Notifications\Handler;
+
+use Bpa\Notifications\Notification\MessageInterface;
+use GuzzleHttp\Client;
+
+/**
+ * Handler for Stride
+ */
+class StrideHandler implements HandlerInterface
+{
+    const URL = 'https://api.atlassian.com/site/{cloudId}/conversation/{roomId}/message';
+
+    /**
+     * @var string
+     */
+    private $cloudId;
+
+    /**
+     * @var string
+     */
+    private $token;
+
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * StrideHandler constructor.
+     *
+     * @param string $cloudId
+     * @param string $token
+     * @param Client $client
+     */
+    public function __construct($cloudId, $token, Client $client)
+    {
+        $this->cloudId = $cloudId;
+        $this->token = $token;
+        $this->client = $client;
+    }
+
+    /**
+     * @param MessageInterface $message
+     *
+     * @return bool|void
+     */
+    public function notify(MessageInterface $message)
+    {
+        $request = $this->client->request(
+            'POST',
+            strtr(self::URL, [
+                '{cloudId}' => $this->cloudId,
+                '{roomId}' => $message->getRoom()->getIdentifier(),
+            ]),
+            [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Authorization' => strtr('Bearer {token}', [
+                        '{token}' => $this->token,
+                    ]),
+                ],
+                'body' => json_encode($this->getContent($message)),
+            ]
+        );
+
+        $this->client->sendRequest($request);
+    }
+
+    /**
+     * @param MessageInterface $message
+     */
+    private function getContent(MessageInterface $message)
+    {
+        $data = [
+            'body' => [
+                'version' => 1,
+                'type' => 'doc',
+                'content' => [],
+            ],
+        ];
+
+        if (null !== $message->getTitle()) {
+            $data['body']['content'][] = [
+                'type' => 'heading',
+                'attrs' => [
+                    'level' => 1,
+                ],
+                'content' => [
+                    [
+                        'type' => 'text',
+                        'text' => $message->getTitle(),
+                    ],
+                ],
+            ];
+        }
+
+        if (null !== $message->getMessage()) {
+            $data['body']['content'][] = [
+                'type' => 'paragraph',
+                'content' => [
+                    [
+                        'type' => 'text',
+                        'text' => $message->getMessage(),
+                    ],
+                ],
+            ];
+        }
+
+        return $data;
+    }
+}
